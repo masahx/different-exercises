3e8c4c6e4518cac3efea13c1fcc26bb077f577a5
masha <spamovski@yahoo.co.uk>
2025-01-31 11:40:28 +0100

    SonarQube said No Issues

--- src/test/java/teplix/MatrixTest.java
+++ src/test/java/teplix/MatrixTest.java
@@ -5,9 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 
  class MatrixTest {
 		
@@ -26,14 +26,17 @@ import org.junit.jupiter.params.provider.CsvSource;
 							new Integer[][]  { {-5,	4,	5,	0}, 
 										 	   {0,	-4,	2,	3 },
 											   {2,	-3,	0,	-2 }, 
-											   {3,	-4,	-1,	-3 }
-							    }		   
+											   {3,	-4,	-1,	-3 },
+							   },
+						    new Integer[1][0],
+							new Integer[0][1]
+							 		   
 										    
 			
 	);
 	
 	@ParameterizedTest
-	@CsvSource(value = {"0,0", "1,2"})
+	@CsvSource(value = {"0,0", "1,2", "3,4"})
 	void transposeSameSqTest(int example, int expected) {
 		
 		Matrix m = new Matrix();
