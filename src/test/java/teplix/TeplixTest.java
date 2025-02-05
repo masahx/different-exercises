f9b65046200af8a8c6224336c0704f10d102c338
masha <spamovski@yahoo.co.uk>
2025-02-05 09:58:43 +0100

    Works, deleted some unused imports

--- src/test/java/teplix/TeplixTest.java
+++ src/test/java/teplix/TeplixTest.java
@@ -5,13 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class TeplixTest {
