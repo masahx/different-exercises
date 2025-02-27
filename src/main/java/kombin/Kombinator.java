f82643787542a438c8bd340ac37a556e261feda8
masha <spamovski@yahoo.co.uk>
2025-02-27 13:17:13 +0100

    Kombinator - prolaze testovi, ima štampanje rezultata

--- src/main/java/kombin/Kombinator.java
+++ src/main/java/kombin/Kombinator.java
@@ -5,22 +5,32 @@ import java.util.stream.Collectors;
 
 public class Kombinator {
 	
-	static final int[] TEN_POW = {1, 10, 100, 1000, 10000};
+	private static final String MAX_LEN_ERROR = "max length for code is 6 digits!";
+	static final int[] TEN_POW = {1, 10, 100, 1000, 10000, 100000};
 	static final String[][] keyboard = new String[10][];
 	
 	static {
-		
-		keyboard[3] = new String[]{"D", "E", "F"};
+				
+		keyboard[0] = 				
+		keyboard[1] = new String[] {"_"};		
+		keyboard[2] = new String[] {"A", "B", "C"};
+		keyboard[3] = new String[] {"D", "E", "F"};
 		keyboard[4] = new String[] {"G", "H", "I"};
 		keyboard[5] = new String[] {"J", "K", "L"};
-		keyboard[9] = new String[] {"W", "X", "Y", "Z"};		
+		keyboard[6] = new String[] {"M", "N", "O"};
+		keyboard[7] = new String[] {"P", "Q", "R", "S"};
+		keyboard[8] = new String[] {"T", "U", "V"};
+		keyboard[9] = new String[] {"W", "X", "Y", "Z"};
 		
 	}
 	
+
+// publishuj Git, pa izbriši štampanje rezultata i opet commituj
+// dodaj Main sledeći put i isprobaj neke šifre
 	
-	String enters(String line) {
+	String enters2(String [] resArray) {
 		
-		return line.replace(",", "\n");
+		return String.join("\n", resArray);
 		
 	}
 	
@@ -29,12 +39,12 @@ public class Kombinator {
 		String [] arrA;
 		String [] arrB;
 		String [] res;
-		if (code > 9) {
+		if (code > 9 && code < 10000000) {
 						
 			int myTen = TEN_POW[(int) Math.log10(code)];
 			arrA = keyboard[code / myTen];
 			arrB = getArray(code % myTen);
-					//keyboard[code % 10];
+			
 			res = new String[arrA.length * arrB.length];
 			
 			for (int i = 0; i < arrA.length; i++) {
@@ -50,9 +60,13 @@ public class Kombinator {
 			System.out.println(code + " x " + Arrays.toString(res));
 			return res;
 			
-		} else {
+		} else if (code <= 9) {
 			
 			return keyboard[code];
+			
+		} else {
+			
+			throw new ArrayIndexOutOfBoundsException(MAX_LEN_ERROR);
 			
 		}
 		
@@ -60,19 +74,8 @@ public class Kombinator {
 	}
 	
 	String decode(int n) {
-//		int n = Integer.parseInt(code);
-//		StringBuilder sb = new StringBuilder();
 		
-		if (n == 43) {
-			
-			return "GD,GE,GF,HD,HE,HF,ID,IE,IF".replace(",", "\n");
-			
-		} else {
-			
-			return Arrays.stream(keyboard[n]).collect(Collectors.joining("\n"));
-			
-		}
-		
+		return enters2(getArray(n));	
 		
 		
 	}
--- src/test/java/kombin/KombinatorTest.java
+++ src/test/java/kombin/KombinatorTest.java
@@ -1,6 +1,8 @@
 package kombin;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
@@ -9,7 +11,6 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,25 +18,40 @@ public class KombinatorTest {
 	
 	Kombinator k = new Kombinator();
 	
-	String enters(String commaIn) {
+	String enters2(String [] inArray) {
 		
-		return commaIn.replace(",", "\n");
+		return String.join("\n", inArray);
 		
 	}
 	
+	String enters(String line) {
+			
+		return line.replace(",", "\n");
+			
+	}
 	
 	@Test 
-	void makeEntersTest() {
+	void makeEnters2Test() {
 		
-		assertEquals(("W\nX\nY\nZ"), enters("W,X,Y,Z"));
+		assertEquals(("W\nX\nY\nZ"), enters2(new String[] {"W", "X", "Y", "Z"}));
+/*		System.out.println(String.join("\n", k.getArray(4945)));
+		System.out.println(String.join("\n", k.getArray(299)));
+		System.out.println(String.join("\n", k.getArray(7323533))); */
+	}
+
+	@ParameterizedTest
+	@MethodSource("enteriString")
+	void makeEntersTest(String expected, String inString) {
+		
+		assertEquals(expected, enters(inString));
 		
 	}
 	
 	@ParameterizedTest
 	@MethodSource("enteriDodaj")
-	void makeEntersKombinTest(String expected, String input) {
+	void makeEntersKombinTest(String expected, String[] inputArr) {
 		
-		assertEquals(expected, k.enters(input));
+		assertEquals(expected, k.enters2(inputArr));
 		
 	}
 	
@@ -53,17 +69,59 @@ public class KombinatorTest {
 				
 		assertEquals(enters(combo), k.decode(n));
 				
-	} 	/* */
+	} 	
+	
+	@Test
+	void tensTest() {
+		
+		assertEquals(1, Kombinator.TEN_POW[0]);
+		
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = {3216544, 1000000})
+	void maxLength6ExceptionTypeTest(int n) {
+		
+		assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> k.decode(n));
+		
+	}
+	
+	
+	@ParameterizedTest
+	@ValueSource(ints = {470789180, 157988})
+	void maxLength6ExceptionMessageTest(int n) {
+		
+		try {
+			
+			k.decode(n);
+			
+		} catch(ArrayIndexOutOfBoundsException e) {
+			
+			assertEquals("max length for code is 6 digits!", e.getMessage());
+			
+		}
+		
+	}
 	
 	private static Stream<Arguments> enteriDodaj() {
 		
 		return Stream.of(
 		
-			Arguments.of("D\nE\nF", "D,E,F"),
-			Arguments.of("S\nT\nX\nV", "S,T,X,V")
+			Arguments.of("D\nE\nF", new String[] {"D", "E", "F"}),
+			Arguments.of("S\nT\nX\nV", new String[] {"S", "T", "X", "V"})
 		
 		);				
 		
+	}
+	
+	private static Stream<Arguments> enteriString() {	
+		
+		return Stream.of(
+			
+			Arguments.of("W\nX\nY\nZ", "W,X,Y,Z"),
+			Arguments.of("D\nE\nF", "D,E,F")
+		);
+	
 	}
 	
 	private static Stream<Arguments> decoder() {	
@@ -73,7 +131,14 @@ public class KombinatorTest {
 			Arguments.of(9, "W,X,Y,Z"),
 			Arguments.of(3, "D,E,F"),
 			Arguments.of(4, "G,H,I"),
-			Arguments.of(43, "GD,GE,GF,HD,HE,HF,ID,IE,IF" )
+			Arguments.of(43, "GD,GE,GF,HD,HE,HF,ID,IE,IF" ),
+			Arguments.of(543, "JGD,JGE,JGF,JHD,JHE,JHF,JID,JIE,JIF,KGD,KGE,KGF,KHD,KHE,KHF,KID,KIE,KIF,LGD,LGE,LGF,LHD,LHE,LHF,LID,LIE,LIF"),
+			Arguments.of(0, "_"),
+			Arguments.of(1, "_"),
+			Arguments.of(2, "A,B,C"),
+			Arguments.of(6, "M,N,O"),
+			Arguments.of(7, "P,Q,R,S"),
+			Arguments.of(8, "T,U,V")
 			
 		);
 	
@@ -94,6 +159,6 @@ public class KombinatorTest {
 		);
 		
 	}
-	/*	*/
+	
 
 }
